701a2c6002fcb5f5ef908275aa7fb5510f6594a3
kaah_kz <karolavila850@gmail.com>
2025-06-24 18:22:28 -0300

    JavaScript assíncrono para buscar dados da API OpenWeather

---
+++ forecastly/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useRef } from 'react'
+import axios from 'axios'
+// import './App.css'
+
+function App() {
+  const [weather, setWheather] = useState({})
+  const inputRef = useRef()
+
+  async function searchCity() {
+    const city = inputRef.current.value
+    const key = 'a649b7af49daf85c4903ee2aa9d0fdf7'
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+
+    const apiInfo = await axios.get(url)
+    setWheather(apiInfo.data)
+
+    // console.log(apiInfo)
+
+  }
+
+  return (
+    <div>
+      <h1>Forecastly - Your weather, simplified.</h1>
+      <input ref={inputRef} type='text' placeholder='Enter the city name'/>
+      <button onClick={searchCity}>Search</button>
+    </div>
+  )
+}
+
+export default App
